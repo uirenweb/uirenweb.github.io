bed0de1e56b04017d61259df1bbb7c8423172e85
Alikhan Tleshov <2wenty2woff@gmail.com>
2024-07-10 21:15:17 +0500

    Updated and replaced hyperlinks for the registration buttons in kz page

--- src/components/pages/kz/Button.tsx
+++ src/components/pages/kz/Button.tsx
@@ -7,14 +7,14 @@ export const Button = () => {
       <div className="sm:flex text-lg md:text-xl font-PublicSans mx-auto max-w-lg justify-between">
         <a
           target="_blank"
-          href="https://docs.google.com/forms/d/1nOXPkkemXU3hsZ8svEwj--GM_uxQDmKuqDaVnVdkeww/edit"
+          href="https://docs.google.com/forms/d/1Qpm5JCqtGffLqoIqEjMUe7pcCJ0sSoBcUpONnjN-2Cc/edit"
           className="block button__tutor cursor-pointer text-slate-800 mb-4 sm:mb-0 px-6 py-4 rounded-3xl sm:mr-3 transition hover:bg-slate-600 hover:text-white"
         >
           Мұғалім болу
         </a>
         <a
           target="_blank"
-          href="https://docs.google.com/forms/d/1Qpm5JCqtGffLqoIqEjMUe7pcCJ0sSoBcUpONnjN-2Cc/edit"
+          href="https://docs.google.com/forms/d/1nOXPkkemXU3hsZ8svEwj--GM_uxQDmKuqDaVnVdkeww/edit"
           className="bg-blue-500 block text-white cursor-pointer button__student sm:ml-3 px-6 py-4 transition rounded-3xl hover:bg-slate-500"
         >
           Оқушыны тіркеу
