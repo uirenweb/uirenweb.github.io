c8cdf758b990de527e3ccda45d9acc10728077b6
Tlesh Ali <2wenty2woff@gmail.com>
2024-09-29 14:37:52 +0500

    updated text intro ru

--- src/components/pages/ru/Intro.tsx
+++ src/components/pages/ru/Intro.tsx
@@ -5,7 +5,7 @@ export const Intro = () => {
     <section id="intro" className="intro bg-darkBlue text-white text-shadow flex items-center">
       <div className="intro__column w-1/2 font-PublicSans py-20 md:py-44 ">
         <div className="text-xl xs:text-2xl sm:text-3xl font-semibold md:text-4xl mx-auto lg:text-5xl w-4/5">
-          <span className="font-bold">UIREN</span> — это некоммерческая организация, которая предоставляет
+          <span className="font-bold">UIREN</span> — это некоммерческая волонтерская инициатива, которая предоставляет
           бесплатное репетиторство и наставничество детям.
         </div>
       </div>
